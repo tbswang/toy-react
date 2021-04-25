f23ecfda537623432868d5f6ed8fe53d55b6622b
tbswang <gt2017qd@163.com>
2021-04-25 11:28:54 +0800

    v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+
+## [0.1.0] - 2021-04-25
+ 
+### Added
+- webpack build config
+- babel transform jsx to js
+- toy react
+  - createElement
+  - render
--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 import { createElement, Component, render } from './toy-react.js';
 
-class MyComponent extends Component {
-  render() {
-    return (
-      <div>
-        <h1>my Component</h1>
-        {this.children}
-      </div>
-    );
-  }
-}
+// class MyComponent extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>my Component</h1>
+//         {this.children}
+//       </div>
+//     );
+//   }
+// }
 
 render(
-  <MyComponent id="a" class="c">
-    <div>acb</div>
-  </MyComponent>,
-  document.body,
+  // <MyComponent id="a" class="c">
+  <div
+    onClick={() => {
+      console.log('a');
+    }}
+  >
+    acb
+  </div>,
+  // </MyComponent>,
+  document.getElementById('root'),
 );
