7e6934abc689e5b60b9759830f43d51b9b667d61
tbswang <gt2017qd@163.com>
2021-04-29 23:39:50 +0800

    feat:webpack config output

--- scripts/webpack.build.js
+++ scripts/webpack.build.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   entry: {
     main: './main.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '..', 'dist'),
   },
   module: {
     rules: [
--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -5,6 +5,10 @@ module.exports = {
   entry: {
     index: './main.js',
   },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname,'..', 'dist'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
      title: 'Development',
